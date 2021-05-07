a9bc26a1d530209cfeb17fab09a1aa547b8b8bdb
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-05-07 11:59:37 +0200

    fix bug

--- auth.js
+++ auth.js
@@ -1,21 +1,21 @@
-const jwtSecret = 'your_jwt_secret'; // 
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
 
-require('./passport');
+require('./passport'); // Your local passport file
 
-const generateJWTToken = (user) => {
-    return jwt.sign(user,jwtSecret, {
-        subject: user.username,
-        expiresIn: '7d',
-        algorithm: 'HS256'
-    });
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.username, //  username you’re encoding in the JWT
+    expiresIn: '7d', //  the token will expire in 7 days
+    algorithm: 'HS256' // algorithm used to “sign” or encode the values of the JWT
+  });
 }
 
-module.exports = (router) => {
+  module.exports = (router) => {
     router.post('/login', (req, res) => {
-      passport.authenticate('local', { session: false }, (error, user) => {
+      passport.authenticate('local', { session: false }, (error, user, info) => {
         if (error || !user) {
           return res.status(400).json({
             message: 'Something is not right',
