70dc06cc651d7499c35165100a808950013241d8
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-07-27 21:18:49 +0200

    changed name reference

--- models.js
+++ models.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 let movieSchema = mongoose.Schema({
     title:    {type: String, required:true},
     description: {type:String, required:true},
-    genre:    [{type:mongoose.Schema.Types.ObjectId, ref:"genre"}],
+    genre:    [{type:mongoose.Schema.Types.ObjectId, ref:"genres"}],
     director:  [{type:mongoose.Schema.Types.ObjectId, ref:"director"}],
     imagePath:  String,
     rating:   String
