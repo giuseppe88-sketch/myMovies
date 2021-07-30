cef18f6b541966ab7cf67874670d4ca3dec1a476
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-07-30 10:47:10 +0200

    delete route

--- index.js
+++ index.js
@@ -99,17 +99,6 @@ app.get('/movies',passport.authenticate('jwt', { session: false }),(req, res) =>
     });
 });
  
-app.get('/users/:username/favorites',passport.authenticate('jwt', { session: false }),(req, res) => {
-  users.favoritesMovies.find()
-    .then((favMov)=>{
-      res.status(201).json(favMov)
-    })
-    .catch((error)=>{
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    })
-})
-
 //defines url documentation and respons whit sendfile
 app.get("/documentation", (req,res) =>{
     res.sendFile("public/documentation.html",{root:__dirname});
